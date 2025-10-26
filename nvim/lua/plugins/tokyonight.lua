022c5a54ee20f99b6024c07d1d7f8716ac4882de
rafalkornel <rafal.kornel123@gmail.com>
2025-10-26 12:20:36 +0100

    implemented neovim basic config with theme

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,42 @@
+-- lua/plugins/colors.lua
+local M = {}
+
+M.plugins = {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- load at startup
+    priority = 1000, -- ensure it loads first
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function()
+      -- Transparent background
+      vim.g.tokyonight_transparent = true
+
+      -- Load colorscheme
+      vim.cmd([[colorscheme tokyonight]])
+
+      -- Apply custom highlights
+      local status_ok, colors = pcall(require, "user.syntax-overrides")
+      if status_ok and colors.apply then
+        colors.apply()
+      end
+
+      -- Reapply highlights if colorscheme changes
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          if status_ok and colors.apply then
+            colors.apply()
+          end
+        end,
+      })
+    end,
+  },
+}
+
+return M.plugins
