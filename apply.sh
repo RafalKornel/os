d15e4be46c24c9a86d0e33399f80d3717ad7e42c
RafalKornel <rafal.kornel123@gmail.com>
2026-05-03 15:07:49 +0200

    add more hyprland config

--- apply.sh
+++ apply.sh
@@ -2,8 +2,12 @@
 
 sudo cp keyd.conf /etc/keyd/default.conf
 
-cp -R hypr ~/.config/hypr/
-cp -R nvim ~/.config/nvim/
+cp -R hypr ~/.config/
+cp -R nvim ~/.config/
+
+cp -R waybar ~/.config/
 
 cp -R zsh ~/.zsh
 echo "source ~/.zsh/alias.zsh" >> ~/.zshrc
+
+sudo cp arch-logo.png /usr/share/plymouth/themes/omarchy/logo.png
