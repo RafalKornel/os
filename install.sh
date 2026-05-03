a55594e0bf9b1ef06def42a236d3f98bb4f45233
RafalKornel <rafal.kornel123@gmail.com>
2026-05-03 16:24:39 +0200

    fix zsh install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-yay -Sy keyd, glow
+yay -Sy keyd, glow, zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/reobin/typewritten.git "$HOME/.zsh/typewritten"
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,1 +1,6 @@
 alias markdown="glow"
+alias l='ls -lh'
+alias la='ls -lAh'
+alias ll='ls -lah'
+alias ls='ls --color=tty'
+alias lsa='ls -lah'
